6d21d43248f8b8fc739971baa8022ef07d79ab44
lalitha456 <98100297+lalitha456@users.noreply.github.com>
2022-02-07 17:08:49 +0530

    assisted practice project

---
+++ CustomExceptions.java
@@ -0,0 +1,17 @@
+package assignment;
+public class CustomExceptions {
+	public static void main(String[] args) {
+		int a = 85, b = 0, result;
+		try {
+			if (b == 0)
+				throw (new ArithmeticException("Can't divide by zero."));
+			else {
+				result = a / b;
+				System.out.print("\n\tThe result is : " + result);
+			}
+		} catch (ArithmeticException Ex) {
+			System.out.print("\n\tError : " + Ex.getMessage());
+		}
+		System.out.print("\n\tEnd of program.");
+	}
+}
